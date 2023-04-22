58a89e6e0c1ef0fba8ce5c56096d384f20f763fd
Peter Samuel <ptahsamuel@gmail.com>
2023-04-22 11:26:09 +0300

    complete project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col'>
         <nav className='flex justify-between items-center w-full mb-10 pt-3'>
             <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
-            <button type='button' onClick={() => window.open('https://github.com/ptahsam')} className='black_btn'>GitHub</button>
+            <button type='button' onClick={() => window.open('https://github.com/ptahsam/article_summarizer')} className='black_btn'>GitHub</button>
         </nav>
 
         <h1 className='head_text'>
